237df550ed32d0f8164a15c22b42dfde91284ab4
apirrone <awanegenebayo@gmail.com>
2023-06-24 14:00:52 +0200

    not working yet. seems that with shell=True, I can't get stdout as well as print it

--- ffmpeg_gpt/__init__.py
+++ ffmpeg_gpt/__init__.py
@@ -1,6 +1,7 @@
-from ffmpeg_gpt.ffmpeg_gpt import get_bash_script
+from ffmpeg_gpt.ffmpeg_gpt import get_bash_script, rerun
 from ffmpeg_gpt.utils import yesno
 import argparse
+import subprocess
 
 
 def main():
@@ -8,14 +9,36 @@ def main():
     argparser.add_argument("prompt", type=str, help="prompt")
     args = argparser.parse_args()
 
+    run_again = True
     script = get_bash_script(args.prompt)
 
-    print("Generated script: ")
-    print("===========")
-    print(script)
-    print("===========")
+    while run_again:
+        print("Generated script: ")
+        print("===========")
+        print(script)
+        print("===========")
 
-    if yesno("Do you want to run this script?"):
-        import subprocess
+        if not yesno("Do you want to run this script?"):
+            print("Cancelling")
+            return
 
-        subprocess.run(script, shell=True)
+        _stdout = ""
+        # history = subprocess.run(script, shell=True)
+        p = subprocess.Popen(
+            script, shell=True, stdout=subprocess.PIPE
+        )  # launch the process
+        while p.poll() is None:  # check if the process is still alive
+            out = p.stdout.readline()  # if it is still alive, grab the output
+            print(out.decode("utf-8"))  # and print it
+            _stdout += out.decode("utf-8")
+
+        print("aaaaaaa")
+        print(_stdout)
+        print("=================================")
+        exit()
+
+        if not yesno("Did it work ?"):
+            if yesno("Do you want to iterate ?"):
+                script = rerun(args.prompt, script, _stdout)
+        else:
+            run_again = False
--- ffmpeg_gpt/ffmpeg_gpt.py
+++ ffmpeg_gpt/ffmpeg_gpt.py
@@ -1,18 +1,56 @@
 import openai
 
+pre_preprompt = """
+You are a bash and ffmpeg expert, you never make mistakes. 
+Write a bash script that uses ffmpeg to perform the following actions.
+Do not explain yourself, just write the script.
+Be sure the script is working. 
+If not specified, the input file is named input.mp4 and the output file is named output.mp4.
+"""
+
+preprompt_normal = (
+    pre_preprompt
+    + """
+Here are the actions requested : 
+
+
+"""
+)
+
+preprompt_rerun = """
+The script you wrote is not working. Here is a full history with the original prompt, the script you wrote and the error message.
+Fix your script, do not explain, do not apologize, just write a new script.
+
+
+"""
+
 
 def get_bash_script(prompt, model="gpt-4"):
-    preprompt = """
-    You are a bash and ffmpeg expert, you never make mistakes. 
-    Write a bash script that uses ffmpeg to perform the following actions.
-    Do not explain yourself, just write the script.
-    Be sure the script is working. 
-    If not specified, the input file is named input.mp4 and the output file is named output.mp4.
-    Here are the actions requested : 
+    prompt = preprompt_normal + prompt
 
+    res = openai.ChatCompletion.create(
+        model=model, messages=[{"role": "user", "content": prompt}]
+    )
+    return res.choices[0].message.content
+
+
+def rerun(prompt, script, stdout, model="gpt-4"):
+    prompt = preprompt_rerun
+    prompt += """
+    Original prompt : 
 
     """
-    prompt = preprompt + prompt
+    prompt += prompt
+    prompt += """
+    Script you wrote :
+
+    """
+    prompt += script
+    prompt += """
+    Error message :
+
+    """
+    prompt += stdout
 
     res = openai.ChatCompletion.create(
         model=model, messages=[{"role": "user", "content": prompt}]
